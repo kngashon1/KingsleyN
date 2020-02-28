52e91efecf8ee37634d4a64c6519c04f91794d09
user <user@DESKTOP-L0S82JM>
2020-02-28 16:22:47 -0600

    first commit

---
+++ GitHubTrial/src/main/java/com/techfios/tester/GitHubTrial/App.java
@@ -0,0 +1,13 @@
+package com.techfios.tester.GitHubTrial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
